4c96b534e2e8dbc4552cf1800c6ffb872ad4c911
Tom Elliott <telliott@hsc.wvu.edu>
2017-10-22 09:35:28 -0400

    Initial Commit

---
+++ RandomPW/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RandomPW
+//
+//  Created by Tom Elliott on 10/22/17.
+//  Copyright © 2017 Tom Elliott. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
