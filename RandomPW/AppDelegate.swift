c2965d235a3431409a0d165410ebf16bfb94f2ad
Tom Elliott <telliott999@gmail.com>
2022-01-21 09:39:23 -0500

    no message

--- RandomPW/AppDelegate.swift
+++ RandomPW/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         window.close()  // default app window
-        let mwc = MainWindowController(windowNibName: NSNib.Name(rawValue: "MainWindowController"))
+        let mwc = MainWindowController(windowNibName: "MainWindowController")
         mwc.showWindow(self)
         // mwc.button_pushed(sender: self)  // display pw on launch
         self.mainWindowController = mwc
